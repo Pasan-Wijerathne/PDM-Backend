329c5a6f57b9663c16c7f8e19e20cb6d295c9235
Pasan-Wijerathne <pasanheshan@gmail.com>
2018-04-21 20:53:49 +0530

    first commit
    first commit

---
+++ src/main/java/com/example/HBMSystem/HbmSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.HBMSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbmSystemApplication.class, args);
+	}
+}
